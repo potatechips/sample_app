8a62c3ec27956e4a89b81cb01c7033bb5d574fff
vagrant <vagrant@localhost.localdomain>
2020-05-08 07:16:32 +0000

    finish3-9

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -11,15 +11,20 @@ class TodolistsController < ApplicationController
   	  @lists = List.all
   end
   def show
-  	  @list =List.find(params[:id])
+  	  @list = List.find(params[:id])
   end
   def edit
-  	  @list =List.find(params[:id])
+  	  @list =  List.find(params[:id])
   end
   def update
   	  list = List.find(params[:id])
   	  list.update(list_params)
   	  redirect_to todolist_path(list.id)
+  end
+  def destroy
+      list = List.find(params[:id])
+      list.destroy
+      redirect_to todolists_path
   end
 private
 
